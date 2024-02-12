e5fab72ce6fcb3a26ba77c868094d78a7d95808c
lugggas <lukas.zenner94@googlemail.com>
2024-02-12 12:39:40 +0100

    Synchronous Code in Observale is synchronously executed

--- website/src/app/check-form/check-form.component.spec.ts
+++ website/src/app/check-form/check-form.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import * as diff from 'diff';
-import { finalize, of } from 'rxjs';
+import { of } from 'rxjs';
 
 import { CheckFormComponent } from './check-form.component';
 import { TestingModule } from '../../testing/testing.module';
@@ -33,14 +33,13 @@ describe('CheckFormComponent', () => {
     const fakeText = 'This iS sme fakee text.';
     const fakeCorrectedText = 'This is some fake text.';
     const fakeDiffs = diff.diffChars(fakeText, fakeCorrectedText);
-    const waitForCheck = new Promise<void>((resolve) => {
-      mockOrthographyService.check.and.returnValue(of({ diffs: fakeDiffs }).pipe(finalize(resolve)));
-    });
+    const fakeCorrectionObservable = of({ diffs: fakeDiffs });
+    mockOrthographyService.check.and.returnValue(fakeCorrectionObservable);
+
     const form = fixture.nativeElement.querySelector('form');
     component.form.setValue({ text: fakeText });
     form.dispatchEvent(new Event('submit'));
 
-    await waitForCheck;
     await fixture.whenStable();
     fixture.detectChanges();
     const actualCorrection = fixture.nativeElement.querySelector('p').innerText;

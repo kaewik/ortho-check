0a916330520c8e3e3744739b8f6be64cbe5d8396
kaewik <32685131+kaewik@users.noreply.github.com>
2024-03-16 14:03:10 +0100

    Make linter more strict

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,9 +15,6 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js', 'karma.conf.js', '**/dist'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
 };
